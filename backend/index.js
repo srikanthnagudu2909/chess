fc6e99053293cc2b3587911698caf2d1217d8a07
srikanth <nagudusrikanth@email.com>
2026-05-07 13:04:10 +0530

    Updated project

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-const cookieParser = require("cookie-parser")
 const { authRouter } = require("./routes/auth.rotues")
 const mongoose = require("mongoose")
 const{Server}=require("socket.io")
@@ -23,7 +22,6 @@ const app = express()
 app.use(cors({origin:["http://localhost:5173"],
   credentials:true}))
 app.use(express.json())
-app.use(cookieParser())
 app.use("/api", authRouter)
 app.use("/api",leaderboardrouter)
 app.post("/api/upload", verifyAuth, parser.single("file"), (req, res) => {
